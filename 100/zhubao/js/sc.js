83add0389fe8aa711fe0e0ea7aae82f5d6db9272
Mr. Zhang <1843078202@qq.com>
2016-06-17 12:20:56 +0800

    hl

---
+++ 100/zhubao/js/sc.js
@@ -0,0 +1,147 @@
+$(function(){
+	
+	$(".by span").click(function(){
+	$(".main_p").stop().slideToggle();
+    if($('.by img').attr('src') == 'images/+_05.png'){
+        $('.by img').attr('src', 'images/fk_03.png');
+    }else{
+        $('.by img').attr('src', 'images/+_05.png');
+    }
+		
+})	
+})
+
+
+
+
+
+$(function(){
+	
+	$(".dnf span").click(function(){
+	$(".xp").stop().slideToggle();
+    if($('.dnf img').attr('src') == 'images/+_05.png'){
+        $('.dnf img').attr('src', 'images/fk_03.png');
+    }else{
+        $('.dnf img').attr('src', 'images/+_05.png');
+    }
+		
+})	
+})
+
+
+
+
+$(function(){
+	
+	$(".cf span").click(function(){
+	$(".linx").stop().slideToggle();
+    if($('.cf img').attr('src') == 'images/+_05.png'){
+        $('.cf img').attr('src', 'images/fk_03.png');
+    }else{
+        $('.cf img').attr('src', 'images/+_05.png');
+    }
+		
+})	
+})
+
+
+
+
+
+$(function(){
+	
+	$(".sw span").click(function(){
+	$(".mac").stop().slideToggle();
+    if($('.sw img').attr('src') == 'images/+_05.png'){
+        $('.sw img').attr('src', 'images/fk_03.png');
+    }else{
+        $('.sw img').attr('src', 'images/+_05.png');
+    }
+		
+})	
+})
+
+
+$(function(){
+	
+	$(".xl span").click(function(){
+	$(".soc").stop().fadeToggle();
+    if($('.xl img').attr('src') == 'images/+_05.png'){
+        $('.xl img').attr('src', 'images/fk_03.png');
+    }else{
+        $('.xl img').attr('src', 'images/+_05.png');
+    }
+		
+})	
+})
+
+
+
+
+
+
+
+
+
+
+$(function(){
+	
+	$(".box span").click(function(){
+	$(".ios").stop().fadeToggle();
+    if($('.box img').attr('src') == 'images/+_05.png'){
+        $('.box img').attr('src', 'images/fk_03.png');
+    }else{
+        $('.box img').attr('src', 'images/+_05.png');
+    }
+		
+})	
+})
+
+
+$(function(){
+	
+	$(".qz span").click(function(){
+	$(".ap").stop().fadeToggle();
+    if($('.qz img').attr('src') == 'images/+_05.png'){
+        $('.qz img').attr('src', 'images/fk_03.png');
+    }else{
+        $('.qz img').attr('src', 'images/+_05.png');
+    }
+		
+})	
+})
+
+
+
+$(function(){
+	$(".pj span").click(function(){
+	$(".ad").stop().fadeToggle();
+    if($('.pj img').attr('src') == 'images/+_05.png'){
+        $('.pj img').attr('src', 'images/fk_03.png');
+    }
+	else{
+        $('.pj img').attr('src', 'images/+_05.png');
+    }	
+})	
+})
+
+
+
+
+
+
+
+$(function(){
+	$(".jg span").click(function(){
+	$(".qa").stop().fadeToggle();
+    if($('.jg img').attr('src') == 'images/+_05.png'){
+        $('.jg img').attr('src', 'images/fk_03.png');
+    }
+	else{
+        $('.jg img').attr('src', 'images/+_05.png');
+    }	
+})	
+})
+
+
+
